56b5e452ad060d15196b50a6d9362b8d293f0275
jf <linzhoufang@gmail.com>
2025-02-03 19:49:20 -0800

    edited example initial g030k8tx

--- examples/SerialMonitorDim/SerialMonitorDim.ino
+++ examples/SerialMonitorDim/SerialMonitorDim.ino
@@ -1,100 +1,76 @@
-/**************
- *  RobotDyn
- *  Dimmer Library
- *  **************
- * 
- * The following sketch is meant to define the dimming value through the serial port of the controller, 
- * using USE_SERIAL.begin 
- *    void printSpace() function is used for adding of space after functional data
- *    void loop()  serial port evaluator, used to register and define values in dimmer.setPower(outVal);
- *    
- * 
- *  ---------------------- OUTPUT & INPUT Pin table ---------------------
- *  +---------------+-------------------------+-------------------------+
- *  |   Board       | INPUT Pin               | OUTPUT Pin              |
- *  |               | Zero-Cross              |                         |
- *  +---------------+-------------------------+-------------------------+
- *  | Lenardo       | D7 (NOT CHANGABLE)      | D0-D6, D8-D13           |
- *  +---------------+-------------------------+-------------------------+
- *  | Mega          | D2 (NOT CHANGABLE)      | D0-D1, D3-D70           |
- *  +---------------+-------------------------+-------------------------+
- *  | Uno           | D2 (NOT CHANGABLE)      | D0-D1, D3-D20           |
- *  +---------------+-------------------------+-------------------------+
- *  | ESP8266       | D1(IO5),    D2(IO4),    | D0(IO16),   D1(IO5),    |
- *  |               | D5(IO14),   D6(IO12),   | D2(IO4),    D5(IO14),   |
- *  |               | D7(IO13),   D8(IO15),   | D6(IO12),   D7(IO13),   |
- *  |               |                         | D8(IO15)                |
- *  +---------------+-------------------------+-------------------------+
- *  | ESP32         | 4(GPI36),   6(GPI34),   | 8(GPO32),   9(GP033),   |
- *  |               | 5(GPI39),   7(GPI35),   | 10(GPIO25), 11(GPIO26), |
- *  |               | 8(GPO32),   9(GP033),   | 12(GPIO27), 13(GPIO14), |
- *  |               | 10(GPI025), 11(GPIO26), | 14(GPIO12), 16(GPIO13), |
- *  |               | 12(GPIO27), 13(GPIO14), | 23(GPIO15), 24(GPIO2),  |
- *  |               | 14(GPIO12), 16(GPIO13), | 25(GPIO0),  26(GPIO4),  |
- *  |               | 21(GPIO7),  23(GPIO15), | 27(GPIO16), 28(GPIO17), |
- *  |               | 24(GPIO2),  25(GPIO0),  | 29(GPIO5),  30(GPIO18), |
- *  |               | 26(GPIO4),  27(GPIO16), | 31(GPIO19), 33(GPIO21), |
- *  |               | 28(GPIO17), 29(GPIO5),  | 34(GPIO3),  35(GPIO1),  |
- *  |               | 30(GPIO18), 31(GPIO19), | 36(GPIO22), 37(GPIO23), |
- *  |               | 33(GPIO21), 35(GPIO1),  |                         |
- *  |               | 36(GPIO22), 37(GPIO23), |                         |
- *  +---------------+-------------------------+-------------------------+
- *  | Arduino M0    | D7 (NOT CHANGABLE)      | D0-D6, D8-D13           |
- *  | Arduino Zero  |                         |                         |
- *  +---------------+-------------------------+-------------------------+
- *  | Arduino Due   | D0-D53                  | D0-D53                  |
- *  +---------------+-------------------------+-------------------------+
- *  | STM32         | PA0-PA15,PB0-PB15       | PA0-PA15,PB0-PB15       |
- *  | Black Pill    | PC13-PC15               | PC13-PC15               |
- *  | BluePill      |                         |                         |
- *  | Etc...        |                         |                         |
- *  +---------------+-------------------------+-------------------------+
- */
+// #include "Arduino.h"
+#include <RBDdimmer.h>
+#include <SoftwareSerial.h>
 
-#include <RBDdimmer.h>//
+// #define USE_SERIAL  SerialUSB // Serial for boards with USB serial port
 
-//#define USE_SERIAL  SerialUSB //Serial for boards whith USB serial port
-#define USE_SERIAL  Serial
-#define outputPin  12 
-#define zerocross  5 // for boards with CHANGEBLE input pins
 
-//dimmerLamp dimmer(outputPin, zerocross); //initialase port for dimmer for ESP8266, ESP32, Arduino due boards
-dimmerLamp dimmer(outputPin); //initialase port for dimmer for MEGA, Leonardo, UNO, Arduino M0, Arduino Zero
+#define outputPin  PA0
+#define zerocross  PA1 // For boards with CHANGEABLE input pins
+// TODO: set serial pins as definitions
+SoftwareSerial mySerial(PA4, PA5);
+String myString = "";
+
+// TODO: add initializations for delay pin..?
+dimmerLamp dimmer(outputPin, zerocross); // Initialize dimmer for ESP8266, ESP32, Arduino Due
+// dimmerLamp dimmer(outputPin); // Initialize dimmer for MEGA, Leonardo, UNO, Arduino M0, Arduino Zero
 
 int outVal = 0;
+int incomingByte = 0; // For incoming serial data
 
 void setup() {
-  USE_SERIAL.begin(9600); 
-  dimmer.begin(NORMAL_MODE, ON); //dimmer initialisation: name.begin(MODE, STATE) 
-  USE_SERIAL.println("Dimmer Program is starting...");
-  USE_SERIAL.println("Set value");
+    mySerial.begin(9600);
+    dimmer.begin(NORMAL_MODE, ON); // Dimmer initialization: name.begin(MODE, STATE)
+    mySerial.println("Dimmer Program is starting...");
+    mySerial.println("Set value");
+    dimmer.setPower(70);
 }
 
-void printSpace(int val)
-{
-  if ((val / 100) == 0) USE_SERIAL.print(" ");
-  if ((val / 10) == 0) USE_SERIAL.print(" ");
+void printSpace(int val) {
+    if ((val / 100) == 0) mySerial.print(" ");
+    if ((val / 10) == 0) mySerial.print(" ");
 }
 
 void loop() {
-  int preVal = outVal;
+    int preVal = outVal;
 
-  if (USE_SERIAL.available())
-  {
-    int buf = USE_SERIAL.parseInt();
-    if (buf != 0) outVal = buf;
-    delay(200);
-  }
-  dimmer.setPower(outVal); // setPower(0-100%);
+    if (mySerial.available()) {
+        int buf = mySerial.read();
+        myString += (char)buf;
 
-  if (preVal != outVal)
-  {
-    USE_SERIAL.print("lampValue -> ");
-    printSpace(dimmer.getPower());
-    USE_SERIAL.print(dimmer.getPower());
-    USE_SERIAL.println("%");
+        if (buf == '\r') {
+            int val = myString.toInt();
 
-  }
-  delay(50);
+            // mySerial.print("Setting dimmer value to: ");
+            // mySerial.println(val);
 
+            if (val < 101) {
+                outVal = val;
+            }
+
+            myString = "";
+            delay(200);
+        }
+    }
+
+    dimmer.setPower(outVal); // setPower(0-100%)
+
+    if (preVal != outVal) {
+        mySerial.print("lampValue -> ");
+        printSpace(dimmer.getPower());
+        mySerial.print(dimmer.getPower());
+        mySerial.println("%");
+    }
+
+    // mySerial.println(dimmer.getMyCounter());
+    // delay(95);
+
+    // while (1) {
+    //     if (mySerial.available() > 0) {
+    //         incomingByte = mySerial.read();
+    //         mySerial.print("I received: ");
+    //         mySerial.println(incomingByte, DEC);
+    //     }
+    //     delay(100);
+    // }
 }
